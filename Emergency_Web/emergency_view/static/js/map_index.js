8744786b9bd8f6052fc103b91dd2dadbeaacda19
Wellien <jbm74700@yahoo.fr>
2019-12-18 11:35:28 +0100

    set_map

---
+++ Emergency_Web/emergency_view/static/js/map_index.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([45.7730904, 4.8410339], 14);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox/streets-v11',
+	accessToken: 'pk.eyJ1Ijoid2VsbGllbiIsImEiOiJjazRiMHhiM3AwOWM5M3NvMnNpZ2U5cnR0In0.4wn-kNouTu3UVKCFaTWfZA'
+}).addTo(map);
