058c662c13d13015fe045c50a46d6b13725dd8f3
Wellien <jbm74700@yahoo.fr>
2019-12-20 08:45:07 +0100

    Modification controller

--- Emergency_Web/emergency_view/controler.py
+++ Emergency_Web/emergency_view/controler.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
 	script_map = url_for('static', filename='js/map_index.js')
 	script_fire = url_for('static', filename='js/getfire.js')
-	return render_template("index.html", map_index = script_map, fire = script_fire)
+	return render_template("index.html", map_index = script_map, fire = script_fire, X=45.7730904, Y=4.8410339, Z=14)
 
 
 if __name__ == "__main__":
--- Emergency_Web/emergency_view/static/js/map_index.js
+++ Emergency_Web/emergency_view/static/js/map_index.js
@@ -1,6 +1,20 @@
-var map = L.map('map').setView([45.7730904, 4.8410339], 14);
-
+//var map = L.map('map').setView([45.7730904, 4.8410339], 14);
+var map = null
 var list_fire = [null]
+
+
+function setmap(x,y,z)
+{
+	map = L.map('map').setView([x, y], z);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox/streets-v11',
+		accessToken: 'pk.eyJ1Ijoid2VsbGllbiIsImEiOiJjazRiMHhiM3AwOWM5M3NvMnNpZ2U5cnR0In0.4wn-kNouTu3UVKCFaTWfZA'
+	}).addTo(map);
+
+}
 
 
 function addFire(x, y, size)
@@ -17,20 +31,15 @@ function addFire(x, y, size)
 
 }
 
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	id: 'mapbox/streets-v11',
-	accessToken: 'pk.eyJ1Ijoid2VsbGllbiIsImEiOiJjazRiMHhiM3AwOWM5M3NvMnNpZ2U5cnR0In0.4wn-kNouTu3UVKCFaTWfZA'
-}).addTo(map);
-
-
-//list_fire.push(addFire(45.77,4.84,10))
-setInterval(function(){ 
+function setfire()
+{
+	setInterval(function(){ 
 
 	fire = get_fire()
-
-
+	
  }, 5000);
+
+}
+
+//list_fire.push(addFire(45.77,4.84,10))
 
