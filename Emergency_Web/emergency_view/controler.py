af527f8aa4431cd8b59ce74c3f4ae5789c79eab3
Wellien <jbm74700@yahoo.fr>
2020-01-06 15:03:43 +0100

    Remove fire

--- Emergency_Web/emergency_view/controler.py
+++ Emergency_Web/emergency_view/controler.py
@@ -3,11 +3,16 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def Lyon():
 	script_map = url_for('static', filename='js/map_index.js')
 	script_fire = url_for('static', filename='js/getfire.js')
-	return render_template("index.html", map_index = script_map, fire = script_fire, X=45.7730904, Y=4.8410339, Z=14)
+	return render_template("index.html", map_index = script_map, fire = script_fire, X=45.7579341, Y=4.8307417, Z=15)
 
+@app.route('/Villerbanne')
+def Villerbanne():
+	script_map = url_for('static', filename='js/map_index.js')
+	script_fire = url_for('static', filename='js/getfire.js')
+	return render_template("index.html", map_index = script_map, fire = script_fire, X=45.7699875, Y=4.8870721, Z=15)
 
 if __name__ == "__main__":
 	app.run()
--- Emergency_Web/emergency_view/static/js/map_index.js
+++ Emergency_Web/emergency_view/static/js/map_index.js
@@ -1,7 +1,7 @@
 //var map = L.map('map').setView([45.7730904, 4.8410339], 14);
 var map = null
-var list_fire = [null]
-
+var list_layout = []
+var list_fire = []
 
 function setmap(x,y,z)
 {
@@ -9,7 +9,6 @@ function setmap(x,y,z)
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		maxZoom: 18,
 		id: 'mapbox/streets-v11',
 		accessToken: 'pk.eyJ1Ijoid2VsbGllbiIsImEiOiJjazRiMHhiM3AwOWM5M3NvMnNpZ2U5cnR0In0.4wn-kNouTu3UVKCFaTWfZA'
 	}).addTo(map);
@@ -17,9 +16,8 @@ function setmap(x,y,z)
 }
 
 
-function addFire(x, y, size)
-{
-	
+function addfire(x, y, size)
+{	
 	var fire = L.circle([x, y], {
 	    color: 'red',
 	    fillColor: '#f03',
@@ -27,18 +25,23 @@ function addFire(x, y, size)
 	    radius: (size * 10)
 	}).addTo(map);
 
-	return fire
-
+	list_layout.push(fire)
 }
 
 function setfire()
 {
+	fires = null
 	setInterval(function(){ 
 
-	fire = get_fire()
-	
+	fires = get_fire()
+
  }, 5000);
 
+}
+
+function removefire(fire)
+{
+	map.removeLayer(fire)
 }
 
 //list_fire.push(addFire(45.77,4.84,10))
