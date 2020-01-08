a7747c14c71fcec6bffa836bb3daa046eacd629b
Simon Suard <sim.suard@gmail.com>
2020-01-08 09:47:07 +0100

    Changement du fichier init .sh

--- src-api/docker/pgsql/init-multiple-databases.sh
+++ src-api/docker/pgsql/init-multiple-databases.sh
@@ -6,12 +6,18 @@ set -u
 function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL	
+		DROP DATABASE $database;    
 	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 }
+
+psql -v ON_ERROR_TOP=1 -U "postgres" <<-EOSQL
+	DROP ROLE $POSTGRES_USER;
+	CREATE ROLE $POSTGRES_USER WITH SUPERUSER CREATEDB LOGIN PASSWORD '$POSTGRES_PASSWORD';
+	CREATE DATABASE $POSTGRES_USER;
+EOSQL
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
