5b9500dc987baa80e9dce53ad870317dafe00bb1
Simon Suard <sim.suard@gmail.com>
2020-01-10 16:06:02 +0100

    Correct bug on receiver

--- Emergency_Web/receiver_uart.py
+++ Emergency_Web/receiver_uart.py
@@ -1,15 +1,15 @@
-import serial, json
+import serial, json, requests
 
 #Ce script python lit les donnees recues sur ttyUSB0 puis les ecrit dans un fichier
 #To do : Envoyer les donnees sur l'API Rest
 #To do : Verifier integrite des donnees
 #A lancer en sudo, a besoin de pyserial pour fonctionner (sudo pip install pyserial)
-
-SERIALPORT = "/dev/ttyUSB0"
+SERIALPORT = "/dev/ttyS3"
+#SERIALPORT = "/dev/ttyUSB0"
 #SERIALPORT = "/dev/tty.usbserial-DA00G4XZ"
 BAUDRATE = 115200
 ser = serial.Serial()
-
+url="http://localhost:8081/api/fires/update-intensity/"
 def initUART():
     # ser = serial.Serial(SERIALPORT, BAUDRATE)
     ser.port=SERIALPORT
@@ -37,10 +37,26 @@ def readUARTMessage():
     if "<" in line or ">" in line:
         return line
 
+def send_to_api(j):
+    jo = json.loads(j)
+    for i in jo:
+        url_api = url+i["x"]+"/"+i["y"]
+        print(url_api)
+        print(i)
+        payload = "{\"intensity\":9}"
+        print("p = "+str(payload))
+
+        x = requests.request("POST",url_api, data=str(payload), headers={'Content-Type': "application/json"})
+        print(x.request.body)
+        print(x.text)
+        #print(i)
+
 def data_to_json(data):
-    file = open("uart.log", "a+")
     json_str = "[\n"
+    data = data.replace('<','')
+    data = data.replace('>','')
     for i in range(len(data)):
+        #print(data[i])
         if data[i] == "(":
             data_dict = {}
             i += 1
@@ -53,6 +69,7 @@ def data_to_json(data):
             json_str += json.dumps(data_dict)+",\n"
     json_str = json_str[:-2] + "\n]"
     print(json_str)
+    file = open("uart.log", "a+")
     file.write(json_str)
     file.close()        
     return json_str
@@ -68,7 +85,7 @@ while True:
         #print(data," ", i)
         total_data += data[7:-2]
         if "(5,9," in total_data: #(5,9, signifie qu'on a recu la derniere ligne
-            data_to_json(total_data)
+            send_to_api(data_to_json(total_data))
             i = 0
             total_data = ""
     i += 1
