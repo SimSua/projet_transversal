bde6d780a023f948ec052b243391b5a125853b78
Sloumuw <guillaume.barillet@cpe.fr>
2020-01-10 16:29:43 +0100

    API - throttle set on api routes to 100 requests per minutes per IP

--- src-api/server/routes/sim.api.php
+++ src-api/server/routes/sim.api.php
@@ -18,32 +18,32 @@ use Illuminate\Http\Request;
 */
 //Route::get('/fires', 'FireController@index');
 
-Route::apiResource('coordinates', 'SimCoordinateController');
-Route::apiResource('fires','SimFireController');
-Route::apiResource('fire-departments','SimFireDepartmentController');
-Route::apiResource('vehicle-types','SimVehicleTypeController');
-Route::apiResource('trucks','SimTruckController');
+Route::apiResource('coordinates', 'SimCoordinateController')->middleware('throttle:100,1');
+Route::apiResource('fires','SimFireController')->middleware('throttle:100,1');
+Route::apiResource('fire-departments','SimFireDepartmentController')->middleware('throttle:100,1');
+Route::apiResource('vehicle-types','SimVehicleTypeController')->middleware('throttle:100,1');
+Route::apiResource('trucks','SimTruckController')->middleware('throttle:100,1');
 
 Route::post('/fires/update-intensity/{id}', function(SimFireController $fireController, int $id) {
     return $fireController->updateIntensity(Request::capture(), $id);
-})->where(['id' => '[0-9]+']);
+})->where(['id' => '[0-9]+'])->middleware('throttle:100,1');
 
 Route::post('/fires/update-intensity/{line}/{column}', function(SimFireController $fireController, int $line, int $column) {
     return $fireController->updateIntensityFromPosition(Request::capture(), $line, $column);
-})->where(['line' => '[0-9]+', 'column' => '[0-9]+']);
+})->where(['line' => '[0-9]+', 'column' => '[0-9]+'])->middleware('throttle:100,1');
 
 Route::get('/coordinates/{line}/{column}', function(SimCoordinateController $coordinateController, int $line, int $column) {
     return $coordinateController->getCoordinateFromGrid($line, $column);
-})->where(['line' => '[0-9]+', 'column' => '[0-9]+']);
+})->where(['line' => '[0-9]+', 'column' => '[0-9]+'])->middleware('throttle:100,1');
 
 Route::get('/fire-departments/vehicles/{id}', function(SimFireDepartmentController $fireDepartmentController, int $id) {
-    return $fireDepartmentController->getAllTrucks($id);
-})->where(['id' => '[0-9]+']);
+    return $fireDepartmentController->getAllTrucks($id)->middleware('throttle:100,1');
+})->where(['id' => '[0-9]+'])->middleware('throttle:100,1');
 
 Route::post('/trucks/assign-fire/{id}', function(SimTruckController $truckController, int $id) {
     return $truckController->assignFire(Request::capture(), $id);
-})->where(['id' => '[0-9]+']);
+})->where(['id' => '[0-9]+'])->middleware('throttle:100,1');
 
 Route::post('/trucks/update-coordinate/{id}', function(SimTruckController $truckController, int $id) {
     return $truckController->updateCoordinate(Request::capture(), $id);
-})->where(['id' => '[0-9]+']);
+})->where(['id' => '[0-9]+'])->middleware('throttle:100,1');
